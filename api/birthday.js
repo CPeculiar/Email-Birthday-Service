69e573b84c0df9d9608b3ad8bb77facdfd4fd69b
CPeculiar <pecudamicable@gmail.com>
2025-04-20 12:54:02 +0100

    fixes 5

--- api/birthday.js
+++ api/birthday.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 module.exports = async (req, res) => {
   try {
     const response = await axios.get('https://email-birthday-service.onrender.com/run-job');
-    res.status(200).json({ message: 'Render endpoint called successfully', result: response.data });
+    res.status(200).json({ message: 'Render endpoint called successfully', result: response.data  });
   } catch (error) {
     console.error('Error:', error.message);
     res.status(500).json({ error: 'Failed to call Render endpoint' });
